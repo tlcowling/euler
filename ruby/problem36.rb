cbaf53d8d10af8da6fd5d92870f2187d09c21eba
Tom Cowling <tom.cowling@gmail.com>
2015-06-17 08:10:57 -0700

    Add 36

--- ruby/problem36.rb
+++ ruby/problem36.rb
@@ -1,33 +1,33 @@
-#Problem 36
-#==================
+#!/usr/bin/env ruby
+
+# Problem 36
 
 =begin
 
-The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
-
-Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
-
-(Please note that the palindromic number, in either base, may not include leading zeros.)
+  The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+ 
+  Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+ 
+  (Please note that the palindromic number, in either base, may not include leading zeros.)
 
 =end
 
 timer_start = Time.now
 
 class String
-	def palindrome?
-		self == self.reverse
-	end
+  def palindrome?
+    self == self.reverse
+  end
 end
 
 sum = 0
 
 (1...1000000).each{
-	|x|
-	if x.to_s(2).palindrome? && x.to_s.palindrome?
-		sum += x
-	end
+  |x|
+  if x.to_s(2).palindrome? && x.to_s.palindrome?
+     sum += x
+  end
 }
 
-p "The sum of all the numbers less than one million that are palindromic in base 10 and base 2 is #{sum}"
-p "It took #{(Time.now-timer_start)*1000} milliseconds"
+puts "36. #{sum} (#{(Time.now-timer_start)*1000} ms)"
 
