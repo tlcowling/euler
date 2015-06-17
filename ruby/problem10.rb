d64595762e73549ff76dc91f1f66fb5981dc5e39
Tom Cowling <tom.cowling@gmail.com>
2015-06-16 22:20:21 -0700

    That one felt too easy, no doubt there are a bunch of optimisations :)

---
+++ ruby/problem10.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+# Problem 10
+
+=begin
+
+  The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+  Find the sum of all the primes below two million.
+
+=end
+require 'mathn'
+
+timer_start = Time.now
+
+sum = Prime.take_while {|p| p < 2000000 }.reduce(:+)
+
+time = (Time.now-timer_start)*1000
+puts "#{sum} (#{time}ms)"
+
