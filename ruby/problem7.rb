82350d0f5c15b3a5b0715d603a971759f3a4c2b0
Tom Cowling <tom.cowling@gmail.com>
2015-06-16 20:04:45 -0700

    I love ruby :)

---
+++ ruby/problem7.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+#Problem 7
+#=====================
+
+=begin
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+
+=end
+
+timer_start = Time.now
+
+# I probably should actually work through this, but I've done it before: Sieve of Eratosthenes and a whole bunch of other algorithms so this time, 
+# its the mathn library again
+require 'mathn'
+p Prime.take(10001).last
+
+puts "This took #{(Time.now-timer_start)*1000} milliseconds"
