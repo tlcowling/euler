56532f8502ded477fedf0377bfcff13e6db879ae
Tom Cowling <tom.cowling@gmail.com>
2015-06-17 07:59:23 -0700

    Add 32 remove 31

--- ruby/problem32.rb
+++ ruby/problem32.rb
@@ -1,22 +1,23 @@
+#!/usr/bin/env ruby
+
 #Problem 32
-#===============
 
 =begin
 
-We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once; for example, the 5-digit number, 15234, is 1 through 5 pandigital.
-
-The product 7254 is unusual, as the identity, 39  186 = 7254, containing multiplicand, multiplier, and product is 1 through 9 pandigital.
-
-Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.
-
-HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
+  We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once; for example, the 5-digit number, 15234, is 1 through 5 pandigital.
+  
+  The product 7254 is unusual, as the identity, 39  186 = 7254, containing multiplicand, multiplier, and product is 1 through 9 pandigital.
+  
+  Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.
+  
+  HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
 
 =end
 require 'prime'
 
 timer_start = Time.now
 
-@@pandigitals = []
+@pandigitals = []
 
 def factors_of(number)
   primes, powers = number.prime_division.transpose
@@ -28,31 +29,28 @@ def factors_of(number)
 end
 
 def pandigital_identity(number)
-
-	identities = []
-	factors_of(number).each{
-		|x|
-		identities << x.join + number.to_s
-	}
-	identities
+  identities = []
+  factors_of(number).each{
+    |x|
+    identities << x.join + number.to_s
+  }
+  identities
 end
 
 def pan_test(num)
-	pandigital_identity(num).each{
-		|id|
-
-		if id.split(//).sort.join == "123456789"
-			@@pandigitals << num
-		end
-	}
+  pandigital_identity(num).each{
+  |id|
+  if id.split(//).sort.join == "123456789"
+    @pandigitals << num
+  end
+}
 end
 
 (1000..10000).each{
-	|x| 
-	pan_test(x)
+  |x| 
+  pan_test(x)
 }
 
-sum = @@pandigitals.uniq.reduce(:+)
+sum = @pandigitals.uniq.reduce(:+)
 
-puts "The sum of the pandigitals are #{sum}"
-p "It took #{(Time.now - timer_start)*1000} milliseconds"
+puts "32. #{sum} (#{(Time.now - timer_start)*1000} ms)"
