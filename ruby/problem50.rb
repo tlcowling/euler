d9a904889b19c0da83a01eaf67ceb605c243fc9d
Tom Cowling <tom.cowling@gmail.com>
2015-06-17 07:46:33 -0700

    Reformat 50 to look like the others

--- ruby/problem50.rb
+++ ruby/problem50.rb
@@ -1,21 +1,37 @@
 #!/usr/bin/env ruby
 
-timer_start = Time.now
+# Problem 50
+
+=begin
+
+  The prime 41, can be written as the sum of six consecutive primes:
+
+  41 = 2 + 3 + 5 + 7 + 11 + 13
+  This is the longest sum of consecutive primes that adds to a prime below one-hundred.
+  
+  The longest sum of consecutive primes below one-thousand that adds to a prime, contains 21 terms, and is equal to 953.
+  
+  Which prime, below one-million, can be written as the sum of the most consecutive primes?
+
+=end
+
 require 'mathn'
+
+timer_start = Time.now
 primes = Prime.each(10000).to_a
 longest = []
 
 primes[0..10].each_with_index {|p, i|
-	count = i
-	begin
-		count += 1
-		consecutivePrimes = primes[i..count]
-		sum = consecutivePrimes.reduce(:+)
-		if sum.prime? && consecutivePrimes.length > longest.length
-			longest = consecutivePrimes
-		end
-	end while sum < 1000000
+  count = i
+  begin
+    count += 1
+    consecutivePrimes = primes[i..count]
+    sum = consecutivePrimes.reduce(:+)
+    if sum.prime? && consecutivePrimes.length > longest.length
+      longest = consecutivePrimes
+    end
+  end while sum < 1000000
 }
 
-puts longest.reduce(:+)
-puts "Elapsed Time: #{(Time.now - timer_start)*1000} milliseconds"
+answer = longest.reduce(:+)
+puts "50. #{answer} (#{(Time.now - timer_start)*1000}) ms"
