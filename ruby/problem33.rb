82335109e6074784c2d63cff929833fdd9f66da0
Tom Cowling <tom.cowling@gmail.com>
2015-06-17 08:05:32 -0700

    Add 34

--- ruby/problem33.rb
+++ ruby/problem33.rb
@@ -1,4 +1,18 @@
-#problem 33
+#!/usr/bin/env ruby
+
+# Problem 33
+
+=begin
+
+  The fraction 49/98 is a curious fraction, as an inexperienced mathematician in attempting to simplify it may incorrectly believe that 49/98 = 4/8, which is correct, is obtained by cancelling the 9s.
+
+  We shall consider fractions like, 30/50 = 3/5, to be trivial examples.
+  
+  There are exactly four non-trivial examples of this type of fraction, less than one in value, and containing two digits in the numerator and denominator.
+  
+  If the product of these four fractions is given in its lowest common terms, find the value of the denominator.
+
+=end
 
 require 'mathn'
 
@@ -9,13 +23,14 @@ i=1
 j=2
 
 until i>99 do
-	until j>99 do
-		fractions.push(i/j)
-		j+=1
-	end
-	j=1
-	i+=1
+  until j>99 do
+    fractions.push(i/j)
+    j+=1
+  end
+  j=1
+  i+=1
 end
 
+sum = 0
 
-p "It took #{(Time.now - timer_start)*1000} milliseconds"
+p "33. #{sum} (#{(Time.now - timer_start)*1000} ms)"
--- ruby/problem34.rb
+++ ruby/problem34.rb
@@ -1,13 +1,14 @@
-#Problem 34
-#=====================
+#!/usr/bin/env ruby
+
+# Problem 34
 
 =begin
 
-145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
-
-Find the sum of all numbers which are equal to the sum of the factorial of their digits.
-
-Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+  145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+  
+  Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+  
+  Note: as 1! = 1 and 2! = 2 are not sums they are not included.
 
 =end
 
@@ -15,20 +16,20 @@ timer_start = Time.now
 
 def factorial(n)
   if n==0 
-  	return 1 
+    return 1 
   end
   1.upto(n).inject(:*)
 end
 
 def sum_of_factorial_of_digits(number)
-	digits = []
-	number.to_s.split(//).each{
-		|n| 
-		digit_factorial = factorial(n.to_i)
-		digits.push(digit_factorial)
-	}
-
-	digits.inject(:+)
+  digits = []
+  number.to_s.split(//).each{
+    |n| 
+    digit_factorial = factorial(n.to_i)
+    digits.push(digit_factorial)
+  }
+  
+  digits.inject(:+)
 end
 
 max = 50000
@@ -36,12 +37,12 @@ i=3
 factorial_digit_sums = []
 
 until i == max
-	if sum_of_factorial_of_digits(i) == i
-		factorial_digit_sums.push(i)
-	end
-	i += 1
+  if sum_of_factorial_of_digits(i) == i
+    factorial_digit_sums.push(i)
+  end
+  i += 1
 end
 
-p "The sum of the numbers whose digit factorials sum to their total is #{factorial_digit_sums.inject(:+)}"
-p "It took #{(Time.now - timer_start)*1000} milliseconds"
+sum = factorial_digit_sums.inject(:+)
+puts "34. #{sum} (#{(Time.now - timer_start)*1000} ms)"
 
